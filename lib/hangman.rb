14382ece87d75ec7fa9255a5e22905dacb11286a
Cory Ribson & David Garber <student@epicodus.com>
2015-05-10 20:18:53 -0700

    checks to see if the letter is int he answer.

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,7 +1,31 @@
+
 class Hangman
-  attr_reader(:answer)
+  attr_reader(:answer, :user_guess)
+
+  @@field = ["_", "_", "_", "_", "_", "_"]
+  @@wrong_guesses = []
+  @@guess_count = 0
 
   def initialize(attributes)
     @answer = attributes[:answer]
+    @answer = @answer.split("")
+    @user_guess = attributes[:user_guess]
   end
+
+  def user_guess(letter)
+    choice = letter
+    if @answer.include?(choice)
+      @answer.each_with_index do |letter, index|
+        if letter == choice
+          @@field[index] = letter
+        end
+      end
+    else
+      @@guess_count = @@guess_count.+(1)
+      @@wrong_guesses.push(letter)
+    end
+    @@guess_count
+    @@field
+  end
+
 end
--- spec/hangman_spec.rb
+++ spec/hangman_spec.rb
@@ -5,8 +5,16 @@ describe(Hangman) do
 
   describe('#answer') do
     it("gives a six letter answer") do
-    test_word = Hangman.new(answer: "horses")
-    expect(test_word.answer()).to(eq("horses"))
+      test_word = Hangman.new(answer: "horses")
+      expect(test_word.answer()).to(eq(["h", "o", "r", "s", "e", "s"]))
+    end
+  end
+
+  describe('#user_guess') do
+    it("checks to see if the letter is in the answer") do
+      test_word = Hangman.new(answer: "horses")
+      choice = "h"
+      expect(test_word.user_guess(choice)).to(eq(["h", "_", "_", "_", "_", "_"]))
     end
   end
 end
